ebaf8f2371afc3853842f3e26c5da84015b9e397
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-19 05:06:28 +0100

    Add DeviceDelete

---
+++ ManagementCentral/Client/Pages/DeviceDelete.razor.cs
@@ -0,0 +1,30 @@
+﻿using ManagementCentral.Client.Services;
+using ManagementCentral.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace ManagementCentral.Client.Pages
+{
+    public partial class DeviceDelete
+    {
+        [Inject]
+        public IDeviceDataService? DeviceDataService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string DeviceId { get; set; }
+        public Device Device { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Device = DeviceDataService.GetDevice(int.Parse(DeviceId));
+        }
+
+        protected void Delete(string DeviceId)
+        {
+            DeviceDataService.DeleteDevice(int.Parse(DeviceId));
+            NavigationManager.NavigateTo($"/listofdevices");
+        }
+    }
+}
