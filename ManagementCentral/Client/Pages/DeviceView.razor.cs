a1cb8f4b641452d852746c935b79e7c27080c36a
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-19 04:39:23 +0100

    Add deviceview

---
+++ ManagementCentral/Client/Pages/DeviceView.razor.cs
@@ -0,0 +1,22 @@
+﻿using ManagementCentral.Client.Services;
+using ManagementCentral.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace ManagementCentral.Client.Pages
+{
+    public partial class DeviceView
+    {
+        [Inject]
+        public IDeviceDataService? DeviceDataService { get; set; }
+
+        [Parameter]
+        public string DeviceId { get; set; }
+        public Device Device { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Device = DeviceDataService.GetDevice(int.Parse(DeviceId));
+            base.OnInitialized();
+        }
+    }
+}
