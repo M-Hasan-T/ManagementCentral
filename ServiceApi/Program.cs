f93c27288bfbd9549864446ef9d51f02a86b87c4
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-20 22:06:12 +0100

    Add DeviceTypeFromApi

--- ServiceApi/Program.cs
+++ ServiceApi/Program.cs
@@ -1,3 +1,5 @@
+using ManagementCentral.Shared.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var specOrigin = "MySpecOrigin";
@@ -52,6 +54,12 @@ app.MapGet("/device", () => "Getting a device from API");
 app.MapGet("/device/{deviceId}/button/{buttonId}",
     (int deviceId, int buttonId) => $"DeviceId {deviceId} and ButtonId {buttonId}");
 
+app.MapGet("/device/{deviceId}", (int deviceId) =>
+{
+    var DeviceService = new DeviceService();
+    return DeviceService.DeviceList[deviceId].DeviceType;
+});
+
 app.UseCors(specOrigin);
 
 app.Run();
@@ -59,4 +67,31 @@ app.Run();
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+class DeviceService
+{
+    public List<Device> DeviceList { get; set; } = new List<Device>();
+
+    public DeviceService()
+    {
+        {
+            DeviceList.Add(new Device()
+            {
+                DeviceId = 1,
+                Location = Location.Sweden,
+                Date = DateTime.Now,
+                DeviceType = "Sensor",
+                Status = Status.online
+            });
+            DeviceList.Add(new Device()
+            {
+                DeviceId = 2,
+                Location = Location.England,
+                Date = DateTime.Now.AddDays(-30),
+                DeviceType = "Machine",
+                Status = Status.offline
+            });
+        }
+    }
 }
