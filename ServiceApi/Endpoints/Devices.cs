ffc84b5f94c9af38f5d4c39c2e7eb313f54b6621
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-12-21 10:15:56 +0100

    Add Devices.cs to Collections and Endpoints folder

---
+++ ServiceApi/Endpoints/Devices.cs
@@ -0,0 +1,44 @@
+﻿using ManagementCentral.Shared.Domain;
+
+namespace DeviceApi.Endpoints
+{
+    public static class Devices
+    {
+        public static void RegisterUserEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var devices = routes.MapGroup("");
+            devices.MapGet("/device", () => "Simple text from API");
+            devices.MapGet("/devices", () => Collections.Devices.DeviceList);
+            devices.MapGet("/devices/{DeviceId}", (int DeviceId) => Collections.Devices.DeviceList
+            .FirstOrDefault(device => device.DeviceId == DeviceId));
+
+            devices.MapPost("/device/add", (Device device) => Collections.Devices.DeviceList.Add(device));
+
+            devices.MapPut("/device/edit/{DeviceId}", (int DeviceId, Device device) =>
+            {
+                Device currentDevice = Collections.Devices.DeviceList.FirstOrDefault(device => device.DeviceId == DeviceId);
+                if (currentDevice != null)
+                {
+                    currentDevice.Location = device.Location;
+                    currentDevice.Status = device.Status;
+                    currentDevice.DeviceType = device.DeviceType;
+                    currentDevice.Date = device.Date;
+                }
+            });
+
+            devices.MapDelete("/device/delete/{DeviceId}", (int DeviceId) =>
+            {
+                var Device = Collections.Devices.DeviceList.FirstOrDefault(device => device.DeviceId == DeviceId);
+                if (Device != null)
+                {
+                    Collections.Devices.DeviceList.Remove(Device);
+                    return Results.Ok("Ok");
+                }
+                else
+                {
+                    return Results.Problem("Error");
+                }
+            });
+        }
+    }
+}
--- ServiceApi/Program.cs
+++ ServiceApi/Program.cs
@@ -1,4 +1,4 @@
-using ManagementCentral.Shared.Domain;
+using DeviceApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,69 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.MapGet("/device", () => "Getting a device from API");
-
-app.MapGet("/device/{deviceId}/button/{buttonId}",
-    (int deviceId, int buttonId) => $"DeviceId {deviceId} and ButtonId {buttonId}");
-
-app.MapGet("/device/{deviceId}", (int deviceId) =>
-{
-    var DeviceService = new DeviceService();
-    return DeviceService.DeviceList[deviceId].DeviceType;
-});
-
+app.RegisterUserEndpoints();
 app.UseCors(specOrigin);
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-class DeviceService
-{
-    public List<Device> DeviceList { get; set; } = new List<Device>();
-
-    public DeviceService()
-    {
-        {
-            DeviceList.Add(new Device()
-            {
-                DeviceId = 1,
-                Location = Location.Sweden,
-                Date = DateTime.Now,
-                DeviceType = "Sensor",
-                Status = Status.online
-            });
-            DeviceList.Add(new Device()
-            {
-                DeviceId = 2,
-                Location = Location.England,
-                Date = DateTime.Now.AddDays(-30),
-                DeviceType = "Machine",
-                Status = Status.offline
-            });
-        }
-    }
-}
